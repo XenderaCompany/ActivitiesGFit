4bfe5ccfe74b364d548874212f614fcf6424b8c4
Dmitrii Salenko <dmitrii.salenko@litslink.com>
2018-12-03 21:19:32 +0200

    Initial commit

---
+++ app/src/main/java/com/dmitriisalenko/gfit/activities/activitiesgfit/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.dmitriisalenko.gfit.activities.activitiesgfit
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.dmitriisalenko.gfit.activities.activitiesgfit.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
