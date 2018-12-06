9b7ba7c242cfdb0a6cb424a174730e5165cab992
Dmitrii Salenko <dmitrii.salenko@litslink.com>
2018-12-06 15:47:40 +0200

    Fixed time period

--- app/src/main/java/com/dmitriisalenko/gfit/activities/activitiesgfit/ui/main/ActivitiesFragment.kt
+++ app/src/main/java/com/dmitriisalenko/gfit/activities/activitiesgfit/ui/main/ActivitiesFragment.kt
@@ -69,6 +69,10 @@ class ActivitiesFragment : Fragment(), AdapterView.OnItemClickListener {
         calendar.time = now
         val endTime = calendar.timeInMillis
         calendar.add(Calendar.MONTH, -3)
+        calendar.set(Calendar.HOUR, 0)
+        calendar.set(Calendar.MINUTE, 0)
+        calendar.set(Calendar.SECOND, 0)
+        calendar.set(Calendar.MILLISECOND, 0)
         val startTime = calendar.timeInMillis
 
 
