24f0131479f4adc02d0388fdcac2a359b8a1f552
Dmitrii Salenko <dmitrii.salenko@litslink.com>
2018-12-06 14:22:15 +0200

    Fixed error

--- app/src/main/java/com/dmitriisalenko/gfit/activities/activitiesgfit/ui/main/ActivityItemView.kt
+++ app/src/main/java/com/dmitriisalenko/gfit/activities/activitiesgfit/ui/main/ActivityItemView.kt
@@ -5,11 +5,15 @@ import android.util.AttributeSet
 import android.widget.LinearLayout
 import com.dmitriisalenko.gfit.activities.activitiesgfit.data.DataActivity
 
-class ActivityItemView(
-    context: Context,
-    attrs: AttributeSet?,
-    defStyleAttr : Int = 0,
-    defStyleRes: Int = 0
-) : LinearLayout(context, attrs, defStyleAttr, defStyleRes) {
+class ActivityItemView : LinearLayout {
     var dataActivity : DataActivity? = null
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs, 0)
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : this(context, attrs, defStyleAttr, 0)
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int):
+            super(context, attrs, defStyleAttr, defStyleRes)
 }
